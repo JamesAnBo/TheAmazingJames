00184c35c5756086613620d24fd269c9612a7166
Cameron Shotter <cameronshotter@gmail.com>
2016-08-12 15:38:00 +1200

    easter egged the crap outta this

--- enemy.js
+++ enemy.js
@@ -3,6 +3,17 @@ var Enemy = function(props) {
   this.health = props.health
   this.minAttackPower = props.minAttackPower
   this.maxAttackPower = props.maxAttackPower
+  var quote = function() {
+    var random = Math.floor(Math.random() * 5)
+    if (random == 1) {
+      return "Beware the SCRUNDGE!"
+    } else if (random == 2) {
+      return "Max is a Scrundge"
+    } else if (random == 3) {
+      return "Mmm, that's a good cuppa joe"
+    }
+  }
+  console.log(quote() || "")
 }
 
 Enemy.prototype.getAttackDamage = function () {
