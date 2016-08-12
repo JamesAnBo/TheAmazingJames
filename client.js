7164105ebd71ac4f70d5f773334f18660fb2df42
Max Robinson <Maxwell.robinson@hotmail.com>
2016-08-12 12:32:22 +1200

    buttons work

--- client.js
+++ client.js
@@ -15,11 +15,14 @@ function start(){
 
   var target = document.getElementById('story-text')
   target.innerHTML = index({name: 'testguy'})
-  var player = new Player({health: 100, attackPower: 50})
-  var testEnemy = new Enemy({health: 65, attackPower: 7})
+  var player = new Player({health: 1000,  minAttackPower: 50, maxAttackPower: 75})
+  var testEnemy = new Enemy({health: 650,  minAttackPower: 15, maxAttackPower: 20})
   while(testEnemy.health > 0){
-    console.log('player health: ', player.health, ' enemy health: ', testEnemy.health)
+    var startingPlayerHealth = player.health
+    var startingEnemyHealth = testEnemy.health
+    console.log('player health: ', startingPlayerHealth, ' enemy health: ', startingEnemyHealth)
     player.attack(testEnemy)
+    console.log('player took ', startingPlayerHealth - player.health ,' damage! Enemy took ',startingEnemyHealth - testEnemy.health, ' damage!')
   } console.log('the enemy was defeated!')
 }
 
